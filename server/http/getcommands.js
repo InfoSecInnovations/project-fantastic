7c2c5fd693584ae92ae62421e804126780f03fce
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-02-19 13:22:57 +0100

    http request console logs

--- server/http/getcommands.js
+++ server/http/getcommands.js
@@ -1,4 +1,6 @@
 const getCommands = (res, req, commands) => {
+  console.log('-----------')
+  console.log('received http request to get command settings...')
   const command_data = Object.entries(commands)
     .map(v => {
       const source = require(`../config/data_sources/${v[0]}`)
@@ -9,6 +11,8 @@ const getCommands = (res, req, commands) => {
       ...result, 
       [v.filename]: {name: v.name || v.filename.slice(0, v.filename.lastIndexOf('.js')), description: v.description, enabled: v.enabled}
     }), {})
+  console.log(`sent metadata for ${Object.keys(command_data).length} commands.`)
+  console.log('-----------')
   res.end(JSON.stringify(command_data))
 }
 
--- server/http/postcommands.js
+++ server/http/postcommands.js
@@ -2,9 +2,15 @@ const GetQuery = require('./getquery')
 
 const postCommands = (res, req, commands) => {
   const query = GetQuery(req)
+  console.log('-----------')
+  console.log('received http request to change command settings...')
   Object.entries(query).forEach(v => { // we should do this instead of just returning the query in case the user sends a bad post request to the server
-    if (commands.hasOwnProperty(v[0])) commands[v[0]] = v[1] === 'true' // all the query values are strings
+    if (commands.hasOwnProperty(v[0])){
+      commands[v[0]] = v[1] === 'true' // all the query values are strings
+      console.log(`${v[0]} command ${commands[v[0]] ? 'enabled' : 'disabled'}`)
+    }
   })
+  console.log('-----------')
   res.end()
   return commands
 }
