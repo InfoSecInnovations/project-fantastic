fc4cfdae3bfcec72e8260f35986a728fbe8667a6
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-04-22 15:59:57 +0200

    remove node-powershell usage, uninstall edge packages

--- packages/fantastic-cli/runpowershell.js
+++ packages/fantastic-cli/runpowershell.js
@@ -1,35 +1,4 @@
-//const Shell = require('node-powershell')
-//const Edge = require('edge-js')
 const { spawn } = require('child_process')
-
-/*const nodePowershell = async (command, log = false) => {
-  const ps = new Shell({
-    executionPolicy: 'Bypass',
-    noProfile: true
-  })
-  ps.addCommand(command)
-  const result = await ps.invoke().catch(rej => {
-    if (log) console.log(`PowerShell command failed: ${rej}`)
-    return ''
-  })
-  ps.dispose()
-  return result
-}*/
-
-/*
-const edge = (command, log = false) => new Promise((resolve, reject) => {
-  const run = Edge.func('ps', command)
-
-  run(command, (e, res) => {
-    if (e) {
-      if (log) console.log(e.message)
-      resolve('')
-    }
-    else {
-      resolve(res[0])
-    }
-  })
-})*/
 
 const child = (command, log = false) => new Promise((resolve, reject) => {
   const child_process = spawn('powershell.exe', [command])
