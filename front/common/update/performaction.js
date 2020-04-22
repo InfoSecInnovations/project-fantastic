a58888534bda0e0083c2342c63385f540c2f63c5
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-04-22 16:11:32 +0200

    fix error when running an action for the first time

--- front/common/update/performaction.js
+++ front/common/update/performaction.js
@@ -1,4 +1,5 @@
 const performAction = (state, action) => {
+  if (!state.action_results.status[action.host]) state.action_results.status[action.host] = {}
   state.action_results.status[action.host][action.action] = 'loading'
 }
 
