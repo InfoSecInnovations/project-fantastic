8f310cdbc5642b7a77e49ca8457715bd76f669f5
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-05-15 16:12:10 +0200

    get-mpcomputerstatus script

--- packages/fantastic-default_actions/index.js
+++ packages/fantastic-default_actions/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   firewall: 'firewall.json',
   getInstalledSoftware: 'getinstalledsoftware.json',
-  /*getMpComputerStatus: require('./getmpcomputerstatus'),
-  getInstalledSoftware: require('./getinstalledsoftware'),
+  getMpComputerStatus: 'getmpcomputerstatus.json',
+  /*getInstalledSoftware: require('./getinstalledsoftware'),
   loggedInUsers: require('./loggedinusers'),
   lastBoot: require('./lastboot')*/
 }
