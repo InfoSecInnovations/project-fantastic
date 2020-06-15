d32886cf29be1efd00552e92ef2da16d0b7d0bee
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-06-15 12:39:56 +0200

    link to logs page when logged in as admin

--- front/main_interface/view/top/index.js
+++ front/main_interface/view/top/index.js
@@ -1,5 +1,6 @@
 const H = require('snabbdom/h').default
 const Search = require('./search')
+const HasRole = require('fantastic-utils/hasrole')
 
 const top = (state, send) => H('div#top', [
   H('h1', "Fantastic"),
@@ -9,6 +10,13 @@ const top = (state, send) => H('div#top', [
       H('div.username', state.user.username),
       H('div.item', `Role: ${state.user.role}`)
     ]),
+    HasRole(state.user, 'admin') ? 
+    H('div.icon_button', {
+      on: {click: e => window.open('/logs.html', '_blank')}
+    }, [
+      H('span.fas fa-history fa-fw'),
+      H('div.label', 'Logs')
+    ]) : undefined,
     H('div.icon_button', {
       on: {click: e => window.open('/auth', '_self')}
     }, [
