ffc4afb39eeffc910ee085a8c930531521d76d0d
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-06-17 15:37:35 +0200

    mac address command

--- packages/fantastic-default_commands/index.js
+++ packages/fantastic-default_commands/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   getDnsClientCache: 'getdnsclientcache.json',
   getHostname: 'gethostname.json',
-  /*getMacAddress: require('./getmacaddress'),
-  getNetIpAddress: require('./getnetipaddress'),
+  getMacAddress: 'getmacaddress.json',
+  /*getNetIpAddress: require('./getnetipaddress'),
   getNetTcpConnection: require('./getnettcpconnection'),
   getOs: require('./getos'),
   nmap: require('./nmap')*/
--- server/commands/runcommand.js
+++ server/commands/runcommand.js
@@ -5,6 +5,7 @@ const runCommand = async (command, hostname) => {
   if (!output) return
   if (command.run.json) return output.map(v => {
     if (typeof command.run.result === 'string') return v[command.run.result]
+    return Object.entries(command.run.result).reduce((result, r) => ({...result, [r[0]]: v[r[1]]}), {})
   })
   else return output
 }
