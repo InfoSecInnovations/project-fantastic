8c8c16412e4e6ea336da0abdc94ee03b223a2277
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-06-29 09:18:04 +0200

    don't allow empty test and quest results to overwrite existing ones

--- front/main_interface/update/questresults.js
+++ front/main_interface/update/questresults.js
@@ -1,4 +1,5 @@
 const questResults = (state, action) => {
+  if (!action.results) continue
   state.quest_results.data[action.quest] = action.results
   state.quest_results.status[action.quest] = 'loaded'
   state.quest_results.date[action.quest] = action.date
--- front/main_interface/update/testresults.js
+++ front/main_interface/update/testresults.js
@@ -1,4 +1,5 @@
 const testResults = (state, action) => {
+  if (!action.results) continue
   state.test_results.data[action.test] = action.results
   state.test_results.status[action.test] = 'loaded'
   state.test_results.date[action.test] = action.date
