e0436e257555ba8349eb3de9ec489ae57d3c4736
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-07-01 16:11:05 +0200

    history log formatting

--- front/main_interface/view/leftpanel/history.js
+++ front/main_interface/view/leftpanel/history.js
@@ -17,7 +17,7 @@ const log_name = (state, log) => {
 const log_content = (state, log) => {
   if (log.event_type == 'test') {
     const parameters = JSON.parse(log.parameters)
-    return H('div.parameters', [
+    return H('div.parameters history_item', [
       H('div.item', 'Parameters used:'),
       H('ul', Object.entries(parameters).map(v => H('li', `${v[0]}: ${v[1]}`)))
     ])
@@ -29,12 +29,22 @@ const history = (state, send) => H('div.scroll_container.panel', [
   H('div.item', [
     H('div.title', 'History')
   ]),
-  H('div.scroll', state.history.results.map(v => H('div.scroll_item', [
+  H('div.scroll', state.history.results.map(v => H('div.scroll_item history_item', [
     H('div.history_title', [
-      H('div.subtitle', headers[v.event_type]),
-      H('div', log_name(state, v))
+      H('div.subtitle history_item', headers[v.event_type]),
+      H('div.subtitle unbold history_item', log_name(state, v))
     ]),
-    H('div.item', log_content(state, v))
+    H('div.item', log_content(state, v)),
+    H('div.item', [
+      H('div.play history_item', {on: {click: () => {}}}, [
+        H('div.item', 'Favorite'),
+        H('span.fas fa-star fa-fw play_button')
+      ]),
+      H('div.play history_item', {on: {click: () => {}}}, [
+        H('div.item', 'Run Again'),
+        H('span.fas fa-play fa-fw play_button')
+      ])
+    ])
   ])))
 ])
 
