08231ff0a14fc0c645ed3df408d433373ee373c2
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-07-22 20:21:04 +0200

    fix followups of followups not displaying properly

--- front/common/update/followupresult.js
+++ front/common/update/followupresult.js
@@ -4,11 +4,11 @@ const followupResult = (state, action) => {
   for (const key of action.followups) {
     action_result = action_result.result.find(v => v.label === key.label).followups[key.followup]
   }
-  if (action.followup && action.result) {
-    action_result.result.find(v => v.label === action.followup.label).followups[action.followup.followup] = action.result.find(v => v.label === action.followup.label).followups[action.followup.followup]
+  if (action.followup) {
+    if (action.result) action_result.result.find(v => v.label === action.followup.label).followups[action.followup.followup] = action.result.find(v => v.label === action.followup.label).followups[action.followup.followup]
   }
   else action_result.result = action.result
-  action_result.foldout = action.result ? true : undefined
+  action_result.foldout = true
   action_result.status = 'loaded'
   action_result.date = action.date
 }
