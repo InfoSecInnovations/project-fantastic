d43fb9ca2b15e46ab1becab4a4623139a93b7069
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-07-24 14:19:33 +0200

    port log details from old hyperapp page

--- front/logs/view/logs/log.js
+++ front/logs/view/logs/log.js
@@ -14,6 +14,45 @@ const log_name = (state, log) => {
   if (log.event_type == 'command') return state.commands[log.command].name
 }
 
-export default (state, send, data) => h('div.log', [
-  h('div.log_details log_name', [h('b', {}, headers[data.event_type]), log_name(state, data)]),
+const test_results = (data, results) => {
+  return {
+    pass: results.every(v => v.result == data.pass.condition),
+    failed: results.filter(v => v.result != data.pass.condition)
+  }
+}
+
+const log_details = (state, log) => {
+  if (log.event_type == 'action') {
+    return [h('div.log_details', [
+      h('div', log.function != 'run' && `Followup action: ${log.function}`),
+      h('div', `Target node: ${log.node_id}`)
+    ])]
+  }
+  if (log.event_type == 'test') {
+    const results = test_results(state.tests[log.test], JSON.parse(log.results))
+    const parameters = JSON.parse(log.parameters)
+    return [
+      h('div.log_details', results.pass ? 'All nodes passed' : `${results.failed.length} nodes failed`),
+      h('div.log_details parameters', [
+        h('div', 'Parameters used:'),
+        h('ul', Object.entries(parameters).map(v => h('li', `${v[0]}: ${v[1]}`)))
+      ])
+    ]
+  }
+  if (log.event_type == 'quest') {
+    const results = test_results(state.quests[log.quest], JSON.parse(log.results))
+    return [h('div.log_details', results.pass ? 'All nodes passed' : `${results.failed.length} nodes failed`)]
+  }
+  if (log.event_type == 'command') {
+    return [h('div.log_details', `${log.status ? 'Enabled' : 'Disabled'}`)]
+  }
+}
+
+export default (state, send, log) => h('div.log', [
+  h('div.log_details log_name', [h('b', headers[log.event_type]), log_name(state, log)]),
+  h('div.log_details', [
+    h('div', log.user.username),
+    h('div', new Date(log.date).toString())
+  ]),
+  ...log_details(state, log)
 ])
