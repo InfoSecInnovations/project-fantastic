766336d02d154d77c09ca504c4d25f291eb75f6d
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-08-05 13:31:07 +0200

    fix logs page controls

--- front/logs/view/search/controls.js
+++ front/logs/view/search/controls.js
@@ -1,16 +1,13 @@
 import {h} from 'snabbdom/h'
 
+const control = (icon, disabled, page, send) => 
+  h(`div.foldout fas fa-${icon} fa-fw`, {
+    class: {disabled},
+    on: {click: disabled ? () => {} : [send, {type: 'page', page}]}
+  })
+
 export default (state, send) => h('div.controls', [
-  h('div.foldout fas fa-step-backward fa-fw', {
-    class: {disabled: !state.page},
-    on: {click: [send, {type: 'page', page: 0}]}
-  }),
-  h('div.foldout fas fa-chevron-left fa-fw', {
-    class: {disabled: !state.page},
-    on: {click: [send, {type: 'page', page: state.page - 1}]}
-  }),  
-  h('div.foldout fas fa-chevron-right fa-fw', {
-    class: {disabled: state.last_page},
-    on: {click: [send, {type: 'page', page: state.page + 1}]}
-  }),
+  control('step-backward', !state.page, 0, send),
+  control('chevron-left', !state.page, state.page - 1, send),
+  control('chevron-right', state.last_page, state.page + 1, send)
 ])
