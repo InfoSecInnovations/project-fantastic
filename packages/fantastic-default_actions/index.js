256cef811d69f7f159c10da40c6145de415c728e
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-08-28 12:35:49 +0200

    delete old packages

--- packages/fantastic-default_actions/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  firewall: 'firewall.json',
-  lastBoot: 'lastboot.json',
-  getInstalledSoftware: 'getinstalledsoftware.json',
-  getMpComputerStatus: 'getmpcomputerstatus.json',
-  loggedInUsers: 'loggedinusers.json'
-}
--- packages/fantastic-default_commands/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  getDnsClientCache: 'getdnsclientcache.json',
-  getHostname: 'gethostname.json',
-  getMacAddress: 'getmacaddress.json',
-  getNetIpAddress: 'getnetipaddress.json',
-  getNetTcpConnection: 'getnettcpconnection.json',
-  getOs: 'getos.json',
-  nmap: 'nmap.json'
-}
