432b1f1409172a236975f0209e29d62d29befff1
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-09-10 11:09:28 +0200

    check actions results agains config file WIP

--- server/actions/runaction/runfunction/index.js
+++ server/actions/runaction/runfunction/index.js
@@ -3,10 +3,14 @@ const ProcessJSON = require('../../../util/processjson')
 const ProcessJSONObject = require('../../../util/processjsonobject')
 const HasRole = require('fantastic-utils/hasrole')
 const IsValid = require('fantastic-utils/isvalid')
+const FS = require('fs-extra')
+const GetConfigPath = require('../../../util/getconfigpath')
+const Path = require('path')
 
-const result = (result_data, output, action, user) => {
+const result = (result_data, output, action, user, filter) => {
   if (Array.isArray(result_data)) return result_data.map(v => result(v, output, action, user))
   return {
+    pass: !filter || filter.some(v => Object.entries(v).every(v => output[v[0]] == v[1])),
     label: ProcessJSON(result_data.label, output),
     data: result_data.data && result_data.data.map(v => ProcessJSON(v, output)).filter(IsValid),
     followups: result_data.followups && result_data.followups.reduce((result, v) => {
@@ -35,11 +39,15 @@ const process_results = results => {
   return results
 }
 
+const get_filter = async filter => GetConfigPath()
+  .then(res => FS.readJson(Path.join(res, filter.file), {throws: false}))
+
 const runFunction = async (action, func, user, hostname, data) => {
   const func_data = action.functions[func]
   const output = await PwshFunction(func_data)(func_data.command, hostname, data)
   if (!func_data.result) return []
-  if (func_data.json) return process_results(output.map(o => result(func_data.result, o, action, user)))
+  const filter = func_data.result.filter ? await get_filter(func_data.result.filter) : undefined
+  if (func_data.json) return process_results(output.map(o => result(func_data.result, o, action, user, filter)))
   return process_results([result(func_data.result, output, action, user)]) // TODO: better handling of non JSON output
 }
 
