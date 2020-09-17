afd247db6e8448ff2917872022e70bc6127a60eb
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-09-17 14:02:24 +0200

    rename function import

--- server/db/addconnections/write/index.js
+++ server/db/addconnections/write/index.js
@@ -2,7 +2,7 @@ const {transaction} = require('../../operations')
 const GetProcessDatabaseID = require('./getprocessdbid')
 const GetLocalIPRowID = require('./getlocaliprowid')
 const GetRemoteRowID = require('./getremoterowid')
-const writeConnection = require('./writeconnection')
+const WriteConnection = require('./writeconnection')
 
 /**
  * Write connection data to database
@@ -22,7 +22,7 @@ const write = async (node_id, connection, date, read_data) => {
   const process_id = await GetProcessDatabaseID(db, node_id, connection.process, read_data.process, read_data.process_name)
   const local_ip_id = await GetLocalIPRowID(db, node_id, date, connection.local_address, read_data.local_ip_row)
   const {id: remote_ip_id, new_nodes} = await GetRemoteRowID(db, node_id, date, connection.remote_address, read_data.remote_ip_row)
-  await writeConnection(db, connection, date, read_data.existing, process_id, local_ip_id, remote_ip_id)
+  await WriteConnection(db, connection, date, read_data.existing, process_id, local_ip_id, remote_ip_id)
   await db.close()
   return new_nodes
 }
