865f67ea57e771f753fb16649556ab5074ab0691
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-10-16 14:39:43 +0200

    move vm2 installation to server module

--- packages/fantastic-utils/isvalid.js
+++ packages/fantastic-utils/isvalid.js
@@ -1,3 +1,7 @@
+/**
+ * Check values for undefined/null while allowing numbers and booleans
+ * @param {*} v 
+ */
 const isValid = v => {
   if (typeof v === 'number' || typeof v === 'boolean') return true
   return v
