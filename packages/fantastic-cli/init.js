1e0faf5fc7f16da0e43de824a2ff8c8cd7cdce73
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-11-17 13:16:40 +0100

    more fantastic-cli init

--- packages/fantastic-cli/init.js
+++ packages/fantastic-cli/init.js
@@ -6,25 +6,29 @@ const DefaultConfig = require('./defaultconfig')
 const Scripts = require('./scripts')
 const Path = require('path')
 
-const cmd = process.platform === 'win32'? 'npm.cmd' : 'npm'
+const npm_cmd = process.platform === 'win32'? 'npm.cmd' : 'npm'
+const npx_cmd = process.platform === 'npx'
 const modules = [
   '@infosecinnovations/project-fantastic',
   '@infosecinnovations/fantastic-default',
   '@infosecinnovations/fantastic-default_auth'
 ]
 
-const init = spawn(cmd, ['init', '-y'], { env: process.env, stdio: 'inherit', detached: true })
-init.on('close', code => {
-  if (code !== 0) return console.error(`npm init failed with exit code ${code}`)
-  const install = spawn(cmd, ['install', ...modules], { env: process.env, stdio: 'inherit', detached: true })
-  install.on('close', code => {
-    if (code !== 0) return console.error(`npm install failed with exit code ${code}`)
-    Promise.all([
-      FS.readJSON('package.json').then(json => FS.writeJSON('package.json', {...json, scripts: {...json.scripts, ...Scripts}})),
-      FS.writeJSON('config.json', DefaultConfig),
-      FS.copy(Path.join(__dirname, 'cert'), 'cert')
-    ])
-    .then(() => console.log('Fantastic installed successfully'))
-    .catch(err => console.error(`Fantastic initialization failed: ${err}`))
+const run_process = (cmd, args, error_message) => new Promise((resolve, reject) => {
+  const spawned = spawn(cmd, args, { env: process.env, stdio: 'inherit', detached: true })
+  spawned.on('close', code => {
+    if (code !== 0) return reject(`${error_message || ''} with exit code ${code}`)
+    resolve()
   })
 })
+
+run_process(npm_cmd, ['init', '-y'], 'npm init failed')
+.then(() => run_process(npm_cmd, ['install', ...modules], 'npm install failed'))
+.then(() => Promise.all([
+  FS.readJSON('package.json').then(json => FS.writeJSON('package.json', {...json, scripts: {...json.scripts, ...Scripts}})),
+  FS.writeJSON('config.json', DefaultConfig),
+  FS.copy(Path.join(__dirname, 'cert'), 'cert')
+]))
+.then(() => run_process(npx_cmd, ['fantastic-upgrade'], 'npx fantastic-upgrade failed'))
+.then(() => console.log('Fantastic installed successfully'))
+.catch(err => console.error(err))
