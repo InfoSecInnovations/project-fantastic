7305cc9036899eea337988b735685ab8ec5d573d
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-11-23 19:26:26 +0100

    ActiveDirectory module asks for AD details if they aren't set

--- packages/fantastic-active_directory/configure.js
+++ packages/fantastic-active_directory/configure.js
@@ -1,4 +1,7 @@
+const GetInput = require('@infosecinnovations/fantastic-utils/getinput')
 const {init} = require('./db')
+const FS = require('fs-extra')
+const Path = require('path')
 
 const defaultGroups = {
   "user": "fantastic-user",
@@ -8,10 +11,18 @@ const defaultGroups = {
 
 const configure = async () => {
   init()
+  const config = await FS.readJSON(Path.join(process.cwd(), 'config.json'))
+  if (!config.authentication.config) config.authentication.config = {}
+  const authConfig = config.authentication.config
+  if (!authConfig.credentials) authConfig.credentials = {}
+  if (!authConfig.credentials.url) authConfig.credentials.url = await GetInput('Enter Active Directory controller URL: ')
+  if (!authConfig.credentials.baseDN) authConfig.credentials.baseDN = await GetInput('Enter Active Directory base DN: ')
 
-  // TODO: ask for AD domain if not configured
-
-  // TODO: ask for AD groups if none are present
+  if (!authConfig.groups) authConfig.groups = {}
+  if (!authConfig.groups.admin) authConfig.groups.admin = await GetInput(`Enter Fantastic admin group name (${defaultGroups.admin}): `) || defaultGroups.admin
+  if (!authConfig.groups.elevated) authConfig.groups.elevated = await GetInput(`Enter Fantastic elevated access group name (${defaultGroups.elevated}): `) || defaultGroups.elevated
+  if (!authConfig.groups.user) authConfig.groups.user = await GetInput(`Enter Fantastic user group name (${defaultGroups.user}): `) || defaultGroups.user
+  await FS.writeJSON(Path.join(process.cwd(), 'config.json'), config)
 }
 
 module.exports = configure
