d90221717b2b8fb3faa330fb4f509216c9f735c7
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-11-23 11:28:46 +0100

    ActiveDirectory module ask for username and password

--- packages/fantastic-active_directory/activedirectory.js
+++ packages/fantastic-active_directory/activedirectory.js
@@ -1,7 +1,7 @@
 const ActiveDirectory = require('activedirectory2').promiseWrapper
 const GetConfig = require('./utils/getconfig')
 
-const activeDirectory = () => GetConfig()
-.then(res => new ActiveDirectory(res.credentials))
+const activeDirectory = (username, password) => GetConfig()
+.then(res => new ActiveDirectory({...res.credentials, username, password}))
 
 module.exports = activeDirectory
