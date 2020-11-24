473003ec9660d23bd75654bbe32dfa1264f403bc
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-11-24 12:49:39 +0100

    ActiveDirectory format hints

--- packages/fantastic-active_directory/configure.js
+++ packages/fantastic-active_directory/configure.js
@@ -15,13 +15,13 @@ const configure = async () => {
   if (!config.authentication.config) config.authentication.config = {}
   const authConfig = config.authentication.config
   if (!authConfig.credentials) authConfig.credentials = {}
-  if (!authConfig.credentials.url) authConfig.credentials.url = await GetInput('Enter Active Directory controller URL: ')
-  if (!authConfig.credentials.baseDN) authConfig.credentials.baseDN = await GetInput('Enter Active Directory base DN: ')
+  if (!authConfig.credentials.url) authConfig.credentials.url = await GetInput('Enter Active Directory controller URL (ldap://dc.domain.com): ')
+  if (!authConfig.credentials.baseDN) authConfig.credentials.baseDN = await GetInput('Enter Active Directory base DN (dc=domain,dc=com): ')
 
   if (!authConfig.groups) authConfig.groups = {}
-  if (!authConfig.groups.admin) authConfig.groups.admin = await GetInput(`Enter Fantastic admin group name (${defaultGroups.admin}): `) || defaultGroups.admin
-  if (!authConfig.groups.elevated) authConfig.groups.elevated = await GetInput(`Enter Fantastic elevated access group name (${defaultGroups.elevated}): `) || defaultGroups.elevated
-  if (!authConfig.groups.user) authConfig.groups.user = await GetInput(`Enter Fantastic user group name (${defaultGroups.user}): `) || defaultGroups.user
+  if (!authConfig.groups.admin) authConfig.groups.admin = await GetInput(`Enter Fantastic admin group name (default: ${defaultGroups.admin}): `) || defaultGroups.admin
+  if (!authConfig.groups.elevated) authConfig.groups.elevated = await GetInput(`Enter Fantastic elevated access group name (default: ${defaultGroups.elevated}): `) || defaultGroups.elevated
+  if (!authConfig.groups.user) authConfig.groups.user = await GetInput(`Enter Fantastic user group name (default: ${defaultGroups.user}): `) || defaultGroups.user
   await FS.writeJSON(Path.join(process.cwd(), 'config.json'), config, {spaces: '\t'})
 }
 
