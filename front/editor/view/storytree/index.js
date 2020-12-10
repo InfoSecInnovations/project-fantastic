8dab0be577569a5a2a27f6595af38383886abad0
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-12-10 18:10:39 +0100

    info panel WIP

--- front/editor/view/storytree/index.js
+++ front/editor/view/storytree/index.js
@@ -2,6 +2,7 @@ import {h} from 'snabbdom/h'
 import Sidebar from './sidebar'
 import ModuleMenu from './modulemenu'
 import Editor from './editor'
+import Info from './info'
 
 export default (state, send) => h('div#story-tree', [
   h('div#sidebar.panel column', Sidebar(state, send)),
@@ -14,15 +15,17 @@ export default (state, send) => h('div#story-tree', [
         if (data) {
           const json = JSON.parse(data)
           const rect = e.target.getBoundingClientRect()
-          send({type: 'editor_node', x: e.clientX - rect.left, y: e.clientY - rect.top, node: json, id: state.editor.jsplumb.uuid()})
+          const id = state.editor.jsplumb.uuid()
+          send({type: 'editor_node', x: e.clientX - rect.left, y: e.clientY - rect.top, node: json, id})
         }
       },
       dragover: e => {
         e.preventDefault()
         e.dataTransfer.dropEffect = 'move'
-      }
+      },
+      click: e => send({type: 'editor_select', id: null})
     }
   }, Editor(state, send)),
-  h('div#info.panel'),
+  h('div#info.panel column', {key: state.editor.selected || 'info'}, Info(state, send)), // setting a key prevents input from carrying over when changing selection
   state.module_menu ? h('div#module-menu.panel', ModuleMenu(state, send)) : undefined
 ])
--- front/editor/view/storytree/sidebar.js
+++ front/editor/view/storytree/sidebar.js
@@ -1,24 +1,24 @@
 import {h} from 'snabbdom/h'
 
-const draggableNode = data => h('li.node', {
+const draggableNode = node => h('li.node', {
   attrs: { draggable: 'true' },
-  on: { dragstart: e => e.dataTransfer.setData('fantastic-data', JSON.stringify(data)) }
-}, h('div.node-label', data.value.name))
+  on: { dragstart: e => e.dataTransfer.setData('fantastic-data', JSON.stringify(node)) }
+}, h('div.node-label', node.data.name))
 
 const nodeList = (state, module, type) => Object.entries(module[type])
   .filter(e => !Object.values(state.editor.nodes).find(node => node.type == type && node.key == e[0]))
-  .map(e => draggableNode({ key: e[0], value: e[1], type }))
+  .map(e => draggableNode({ key: e[0], data: e[1], type }))
 
 export default (state, send) => [
   h('div.button', {on: {click: e => send({type: 'load_module_menu', enabled: true})}}, 'Load module'),
   ...(state.modules ? state.modules.map(module => [
     h('h2', module.name),
     h('div.sidebar-columns', [
-      h('div.column', [
+      h('div.column center', [
         h('h3', 'Actions'),
         h('ul', nodeList(state, module, 'actions'))
       ]),
-      h('div.column', [
+      h('div.column center', [
         h('h3', 'Tests'),
         h('ul', nodeList(state, module, 'tests'))
       ])
