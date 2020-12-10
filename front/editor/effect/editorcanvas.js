b5d005d7482912b0de0b9d25742eb578c873843f
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-12-10 15:05:32 +0100

    port even listeners from PoC

--- front/editor/effect/editorcanvas.js
+++ front/editor/effect/editorcanvas.js
@@ -9,4 +9,17 @@ export default (state, action, send) => jsPlumb.ready(() => {
     container: action.id
   })
   send({type: 'editor_jsplumb', instance})
+  // prevent duplicate connections
+  instance.bind('beforeDrop', info => !instance.select({source: info.sourceId, target: info.targetId}).length) 
+  // if we establish a new connection flowing the opposite way to an existing one we should remove the existing one
+  instance.bind('connection', info => instance.select({source: info.targetId, target: info.sourceId}).deleteAll())
+  // prevent dragging outside the canvas      
+  instance.bind('drag:move', info => {
+    const parentRect = instance.getContainer().getBoundingClientRect()
+    const childRect = info.el.getBoundingClientRect()
+    if (childRect.right > parentRect.right) info.el.style.left = `${parentRect.width - childRect.width}px`
+    if (childRect.left < parentRect.left) info.el.style.left = '0px'
+    if (childRect.top < parentRect.top) info.el.style.top = '0px'
+    if (childRect.bottom > parentRect.bottom) info.el.style.top = `${parentRect.height - childRect.height}px`
+  })
 })
