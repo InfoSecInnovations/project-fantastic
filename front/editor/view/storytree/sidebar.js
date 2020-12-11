1d2f00a81819d275855aedec4be9b1f7933ef74a
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-12-11 15:47:11 +0100

    save module paths relative to save file

--- front/editor/view/storytree/sidebar.js
+++ front/editor/view/storytree/sidebar.js
@@ -7,7 +7,7 @@ const draggableNode = node => h('li.node', {
 
 const nodeList = (state, module, type) => Object.entries(module[type])
   .filter(e => !Object.values(state.editor.nodes).find(node => node.type == type && node.key == `${module.name}/${e[0]}`))
-  .map(e => draggableNode({ key: `${module.name}/${e[0]}`, data: e[1], type }))
+  .map(e => draggableNode({ key: `${module.name}/${e[0]}`, data: e[1], type, path: module.path }))
 
 export default (state, send) => state.modules ? state.modules.map(module => [
     h('h2', module.name),
