5313867fbadfe5af13e292f5c83c7d1c484db143
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-12-11 15:28:35 +0100

    save file functionality

--- front/editor/effect/loadmodule.js
+++ front/editor/effect/loadmodule.js
@@ -5,12 +5,15 @@ const loadDir = path => FS.readdir(path)
   .then(files => Promise.all(
     files
     .filter(file => file.endsWith('.json'))
-    .map(file => FS.readJSON(Path.join(path, file)).then(json => ({key: file, value: json})))
+    .map(file => FS.readJSON(Path.join(path, file)).then(json => ({key: file.slice(0, file.lastIndexOf('.json')), value: json})))
   ))
   .then(files => files.reduce((result, file) => ({...result, [file.key]: file.value}), {}))
 
 export default async (state, action, send) => {
-  const actions = await loadDir(Path.join(process.cwd(), '../packages', action.module, 'actions'))
-  const tests = await loadDir(Path.join(process.cwd(), '../packages', action.module, 'tests'))
-  send({type: 'add_module', module: {name: action.module, actions, tests}})
+  const packageJSON = await FS.readJSON(Path.join(action.module, 'package.json')).catch(() => undefined)
+  if (!packageJSON) return alert('This directory is not a valid package. Use npm init to create a package.json file.')
+  const actions = await loadDir(Path.join(action.module, 'actions')).catch(() => ({}))
+  const tests = await loadDir(Path.join(action.module, 'tests')).catch(() => ({}))
+  const name = packageJSON.name
+  send({type: 'add_module', module: {name, path: action.module, actions, tests}})
 }
