c2ded47af75806ae1e021bb4f38a60d0c59ed511
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-12-22 14:50:02 +0100

    minor fixes

--- front/main_interface/view/leftpanel/quests/story.js
+++ front/main_interface/view/leftpanel/quests/story.js
@@ -2,7 +2,7 @@ import {h} from 'snabbdom/h'
 
 export default (state, send) => [
   h('h2.panel_title', 'Story Quests'),
-  h('div.scroll spaced', Object.entries(state.stories).map(v => h('div.scroll_item', [
-    h('h3', {on: {click: e => send({type: 'select_story', story: v[0]})}}, v[1].name)
+  h('div.scroll', Object.entries(state.stories).map(v => h('div.scroll_item', [
+    h('div.button', {on: {click: e => send({type: 'select_story', story: v[0]})}}, v[1].name)
   ])))
 ]
