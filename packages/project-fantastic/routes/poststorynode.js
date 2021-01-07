706a4e28caed9601959654c57b6741de354e848e
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-01-07 16:43:32 +0100

    story node route returns result

--- packages/project-fantastic/routes/poststorynode.js
+++ packages/project-fantastic/routes/poststorynode.js
@@ -3,13 +3,16 @@ const {transaction} = require('../db')
 
 const postStoryNode = async (user, res, req, query, stories) => {
   console.log(`postStoryNode: Received http request to run node ${query.node} from story ${query.story}`)
+  const date = Date.now()
   const db = await transaction()
-  const result = await RunStoryNode(db, query.story, query.node, user, Date.now())
+  // TODO: validate query
+  const result = await RunStoryNode(db, query.story, query.node, user, date)
+  await db.insert('all_history', {event_type: 'story', event_id: result.event_id, date, user_id: user.user_id})
   await db.close()
   // TODO: check if we completed previous node
-  // TODO: run action or test on selection
-  // TODO: return node result
-  res.end()
+  if (res.aborted) return
+  res.end(JSON.stringify({result: result.results, rows: result.rows, date}))
+  console.log(`postStoryNode: completed node ${query.node} from story ${query.story}, queried ${result.rows.length} nodes`)
 }
 
 module.exports = postStoryNode
