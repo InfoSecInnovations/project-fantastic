c23adf5a5b8e66a421be93d8fc6e4d8bf09f7837
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-01-12 18:00:10 +0100

    correctly handle action node execution

--- front/main_interface/view/selection/multiactions.js
+++ front/main_interface/view/selection/multiactions.js
@@ -1,25 +1,18 @@
 import {h} from 'snabbdom/h'
-import HostString from '@infosecinnovations/fantastic-front/util/hoststring'
+import MultiAction from '@infosecinnovations/fantastic-front/view/actions/multiaction'
 
 export default (state, send) => {
   if (!state.actions) return
   const nodes = state.selected.nodes.map(v => state.nodes[v])
-  const actions = Object.entries(state.actions).filter(v => v[1].hosts.includes('none') || nodes.every(n => v[1].hosts.includes(n.access)))
+  const actions = Object.entries(state.actions).filter(v => v[1].hosts.includes('none') || nodes.every(n => v[1].hosts.includes(n.access))).map(v => v[0])
   return h('div.scroll_container',
-    h('div.scroll spaced', !actions.length ? h('div.scroll_item', 'No actions compatible with these hosts') : actions.map(v => h('div.scroll_item spaced', [
-      h('div.item', [
-        h('h3', v[1].name),
-        h('div.button', 
-          { 
-            on: {click: [
-              [send, {type: 'open_viewer', nodes: state.selected.nodes}],
-              () => nodes.forEach(n => send({type: 'perform_action', action: v[0], node_id: n.node_id, host: n.hostname}))
-            ]},
-          }, 
-          'Run')
-      ]),
-      v[1].description ? h('div.item', v[1].description) : undefined,
-      h('div.targets', [h('b', 'Valid targets:'), ` ${v[1].hosts.map(HostString).join(', ')}.`]),
-    ])))
+    h('div.scroll spaced', 
+      !actions.length ? 
+      h('div.scroll_item', 'No actions compatible with these hosts') : 
+      actions.map(v => MultiAction(state, v, e => {
+        send({type: 'open_viewer', nodes: state.selected.nodes})
+        nodes.forEach(n => send({type: 'perform_action', action: v[0], node_id: n.node_id, host: n.hostname}))
+      }))
+    )
   )
 }
