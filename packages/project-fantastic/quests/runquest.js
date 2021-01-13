f61aeb1162200e2c075f774a5af89420438c96e9
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-01-13 17:05:52 +0100

    select all hosts if age isn't specified for quest selection

--- packages/project-fantastic/quests/runquest.js
+++ packages/project-fantastic/quests/runquest.js
@@ -12,7 +12,8 @@ const ConvertTime = require('@infosecinnovations/fantastic-utils/converttime')
  */
 const runQuest = async (db, quest, user, date) => {
   const test_obj = await GetPackagedData(quest, 'tests')
-  const rows = await getNodes({date: Date.now() - ConvertTime(test_obj.quest.selection.age), access: test_obj.hosts})
+  const age = ConvertTime(test_obj.quest.selection.age)
+  const rows = await getNodes({date: age && Date.now() - age, access: test_obj.hosts})
   const row_ids = rows.map(v => v.node_id)
   const event_id = await db.insert('quest_history', {quest, date, user_id: user.user_id, rows: JSON.stringify(row_ids)})
   const results = (await RunTest(db, quest, user, date, row_ids, test_obj.quest.parameters, event_id)).results
