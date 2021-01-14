711525edefd0e874f5ff44315c9bcc0c8198235a
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-01-14 10:33:00 +0100

    restructuring test view WIP

--- packages/fantastic-front/view/test/index.js
+++ packages/fantastic-front/view/test/index.js
@@ -1,10 +1,9 @@
 import {h} from 'snabbdom/h'
-import TimeAgo from '../../util/timeago'
 import Result from './result'
 import Info from './info'
 import PlayButton from './playbutton'
 
-export default (state, send, name, data, parameters, result_data, result_date, result_parameters, result_approval, loading, play_action, options = {}) => {
+export default (state, send, name, data, parameters, result_data, result_date, result_parameters, result_approval, loading, play_action, {success_prefix, type} = {}) => {
   const results = result_date > Date.now() - 1000 * 60 * 60 * 24 && result_data // TODO: maybe we want to be able to define a custom maximum result age
   const pass = results && (data.pass === 'review' ? result_approval : results.every(r => r.result == data.pass.condition))
   const failed_results = results && data.pass !== 'review' ? results.filter(r => r.result != data.pass.condition) : []
@@ -17,13 +16,19 @@ export default (state, send, name, data, parameters, result_data, result_date, r
       loading ? 'loading' : valid_parameters ? {on: {click: [send, play_action]}} : {class: {waiting: true}}, 
       parameters && parameters.edit && parameters.edit()
     ),
-    // TODO: make this block into its own file
-    ...(results ?
-    [
-      h('h4', `Results from ${TimeAgo(result_date)}`),
-      parameters && parameters.result && parameters.result(),
-      // TODO: is_quest looks like a bool here, but the expected parameter is a string?
-      Result(send, name, data, pass, options, result_parameters, failed_nodes, results, options && options.is_quest)
-    ] : [])
+    ...(results ? 
+      Result(
+        send, 
+        parameters && parameters.result && parameters.result(), 
+        name, 
+        data,
+        result_date,
+        pass, 
+        success_prefix, 
+        result_parameters, 
+        failed_nodes, 
+        results,
+        type)
+    : [])
   ])
 }
