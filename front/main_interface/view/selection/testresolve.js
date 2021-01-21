3631fc6e9f0564e7a60363dbc3455500afd6d295
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-01-21 12:22:21 +0100

    check patch version action and test, Windows Defender test

--- front/main_interface/view/selection/testresolve.js
+++ front/main_interface/view/selection/testresolve.js
@@ -1,18 +1,24 @@
 import FormatString from '@infosecinnovations/fantastic-utils/formatstring'
 import {h} from 'snabbdom/h'
 
+const action_view = (state, data) =>  {
+  const action_obj = state.actions[data.path]
+  return [
+    h('h4', `${action_obj.name}${data.function ? `: ${action_obj.names[data.function]}` : ''}`),
+    h('pre', FormatString(action_obj.commands[data.function || 'run'], data.data))
+  ]
+}
+
 export default (state, send) => {
   const test_obj = state.tests[state.test_resolve.test]
   const action_data = test_obj.pass.failure.action
-  const action_obj = state.actions[action_data.path]
   return h('div.scroll_container', h('div.scroll spaced', [
     h('div.item', [
-      h('h3', `${action_obj.name}${action_data.function ? `: ${action_obj.names[action_data.function]}` : ''}`),
+      h('h3', `Resolve issues found by ${test_obj.name}`),
       state.test_results.status[state.test_resolve.test] == 'loading' ? h('div.button disabled', 'Running...') :
       h('div.button', {on: {click: e => send({type: 'run_test_resolve', test_id: state.test_resolve.test_id, test: state.test_resolve.test})}}, 'Run')
     ]),
-    h('pre', FormatString(action_obj.commands[action_data.function || 'run'], action_data.data)),
-    h('div', `Run this to resolve issues found by ${test_obj.name}`)
+    ...(Array.isArray(action_data) ? action_data.map(data => action_view(state, data)) : [action_view(state, action_data)])
     // TODO: show which quest/story we're resolving
   ]))
 }
