d60167ff480d853f5cdf7d443cbe47960377b785
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-03-01 18:24:59 +0100

    TODO notes on generating history items

--- packages/project-fantastic/routes/postactions.js
+++ packages/project-fantastic/routes/postactions.js
@@ -14,7 +14,8 @@ const postActions = async (user, res, req, query, actions) => {
   const db = await transaction()
   const opts = query.connection && {data: await GetConnectionData(db, query.connection)}
   const result = await RunAction(db, query.action, 'run', query.node_id, user, date, opts)
-  await db.insert('all_history', {event_type: 'action', event_id: result.event_id, date, user_id: user.user_id})
+  await db.insert('all_history', {event_type: 'action', date, user_id: user.user_id, event_id: result.event_id})
+  // TODO: GetData to generate history item
   await db.close()
   if (res.aborted) return
   res.end(JSON.stringify({result: result.result.results, filter: result.result.filter, date}))
