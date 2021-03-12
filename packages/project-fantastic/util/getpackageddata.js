e74510bdadd16e9503709bb52304ed72dc791264
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-03-12 17:45:34 +0100

    stories route query validation

--- packages/project-fantastic/util/getpackageddata.js
+++ packages/project-fantastic/util/getpackageddata.js
@@ -11,8 +11,11 @@ const getPackagedData = async (path, data_type) => {
   const splitIndex = path.lastIndexOf('/')
   if (splitIndex == -1) return
   const fileName = path.substring(splitIndex + 1)
+  // TODO: is there some way to use require.resolve to make this more robust?
   const result = await FS.readFile(Path.join('node_modules', path.substring(0, splitIndex), data_type, `${fileName}.json`))
   .then(res => JSON.parse(res))
+  .catch(() => undefined)
+  if (!result) return
   if (!result.name) result.name = fileName
   if (data_type == 'stories' && (!result.questConfig.selection.age || !Object.keys(result.questConfig.selection.age).length)) result.questConfig.selection.age = {d: 1}
   if (data_type == 'quests' && (!result.selection.age || !Object.keys(result.selection.age).length)) result.selection.age = {d:1}
