9431579b287377a475658e4227024bba9f07aebb
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-03-16 14:23:16 +0100

    version argument in CLI installer

--- packages/fantastic-cli/init.js
+++ packages/fantastic-cli/init.js
@@ -21,20 +21,24 @@ const auth_modules = [
 
 const run = async () => {
   try {
+    const args = process.argv.slice(2, process.argv.length)
+    let tag = args[0] || '@latest'
+    if (!tag.startsWith('@')) tag = `@${tag}`
     await RunProcess(npm_cmd, ['init', '-y'], 'npm init failed')
     // we want to display 1 based indices in the command line but use 0 based to access the array
     const custom = auth_modules.length + 1
     let use_auth = await GetInput(`Select authentication module:\n${auth_modules.map((m, i) => `${i+1} - ${m}`).join('\n')}\n${custom} - Custom\n`)
     while (!use_auth || isNaN(parseInt(use_auth)) || parseInt(use_auth) > custom) use_auth = await GetInput('Please enter a valid choice!')
     const auth_module = parseInt(use_auth) != custom ? auth_modules[parseInt(use_auth) - 1] : await GetInput('Enter module name: ')
-    await RunProcess(npm_cmd, ['i', ...modules, auth_module], 'npm install failed')
+    await RunProcess(npm_cmd, ['i', ...modules.map(m => `${m}${tag}`), auth_module], 'npm install failed')
+    const module_path = auth_module.replace(/.(@.+)/, (match, p1) => match.replace(p1, '')) // strip out version from module name if there is one
     await Promise.all([
       FS.readJSON('package.json').then(json => FS.writeJSON('package.json', {...json, scripts: {...json.scripts, ...Scripts}})),
-      FS.writeJSON('config.json', {...DefaultConfig, authentication: {module: auth_module}}, {spaces: '\t'}),
+      FS.writeJSON('config.json', {...DefaultConfig, authentication: {module: module_path}}, {spaces: '\t'}),
       FS.copy(Path.join(__dirname, 'cert'), 'cert')
     ])
     await RunProcess(npx_cmd, ['fantastic-upgrade'], 'npx fantastic-upgrade failed')
-    const module_path = auth_module.replace(/.(@.+)/, (match, p1) => match.replace(p1, '')) // strip out version from module name if there is one
+
     const auth = AuthFactory(require(Path.join(process.cwd(), 'node_modules', module_path)))
     if (auth.configure) await auth.configure()
     console.log('Fantastic installed successfully')
