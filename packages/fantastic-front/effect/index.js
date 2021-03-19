45110199420ab7a83e3e8a4c3d4874dc847c7623
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-03-19 13:49:03 +0100

    add module info metadata object support

--- packages/fantastic-front/effect/index.js
+++ packages/fantastic-front/effect/index.js
@@ -2,6 +2,7 @@ import ActionFollowup from './actionfollowup'
 import GenerateQuery from './generatequery'
 
 export default (state, action, send) => {
+  if (action.type == 'init') fetch('/module_info').then(res => res.json()).then(res => send({type: 'module_info', info: res}))
   if (action.type == 'perform_action') fetch(`/actions?${GenerateQuery({action: action.action, node_id: action.node_id, connection: action.connection})}`, {method: 'POST'})
     .then(res => res.json())
     .then(res => {
