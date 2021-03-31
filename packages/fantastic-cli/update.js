2404a817fdc72350a756c800486fc64188c4b181
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-03-31 21:23:04 +0200

    fantastic-cli update command

--- packages/fantastic-cli/update.js
+++ packages/fantastic-cli/update.js
@@ -2,6 +2,17 @@
 
 const FS = require('fs-extra')
 const IsInstalled = require('is-installed')
+const RunProcess = require('@infosecinnovations/fantastic-utils/runprocess')
+
+const npm_cmd = process.platform === 'win32'? 'npm.cmd' : 'npm'
+const npx_cmd = process.platform === 'win32'? 'npx.cmd' : 'npm'
+const base_module = '@infosecinnovations/project-fantastic'
+
+const isiModules = [
+  '@infosecinnovations/fantastic-default',
+  '@infosecinnovations/fantastic-default_auth',
+  '@infosecinnovations/fantastic-active_directory'
+]
 
 const run = async () => {
   try {
@@ -15,9 +26,13 @@ const run = async () => {
     if (!fantastic_installed) return console.log("Couldn't find Fantastic installation, please install Fantastic first using npx fantastic-cli init!")
     const has_config = await FS.pathExists('config.json')
     if (!has_config) return console.log("Fantastic installation was found, but couldn't find config.json, please initialize Fantastic first using npx fantastic-cli init!")
-    // npm i project-fantastic@tag auth@tag
+    await RunProcess(npm_cmd, ['i', `${base_module}${tag}`], 'npm i failed for Fantastic base module')
+    const packageJSON = await FS.readJSON('package.json')
+    for (const m of isiModules) {
+      if (packageJSON.dependencies[m]) await RunProcess(npm_cmd, ['i', `${m}${tag}`], `npm i ${m} failed`)
+    }
     // check if any of the ISI modules are installed and update those if so
-    // run npx fantastic-upgrade
+    await RunProcess(npx_cmd, ['fantastic-upgrade'], 'npx fantastic-upgrade failed')
     console.log(`Updated Fantastic and dependencies to ${tag} tag.`)
   }
   catch (err) {
