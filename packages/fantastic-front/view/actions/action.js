3432dc8b0ceb02356876694cd4834a80390e13a9
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-04-08 12:41:14 +0200

    clickable action result header foldouts

--- packages/fantastic-front/view/actions/action.js
+++ packages/fantastic-front/view/actions/action.js
@@ -4,6 +4,7 @@ import Result from './result'
 import TimeAgo from '../../util/timeago'
 import Run from './run'
 import Command from './command'
+import InnerFoldout from '../innerfoldout'
 
 export default (state, send, node, connection, action, action_data) => {
   const result_data = state.action_results[node.hostname] && state.action_results[node.hostname][action]
@@ -25,27 +26,19 @@ export default (state, send, node, connection, action, action_data) => {
       }
     },
     key: id
-  }, [
-    h('input.auto_foldout', {
-      attrs: {type: 'checkbox', id, checked: state.foldout_checkboxes[id]},
-      on: {input: e => send({type: 'foldout_checkbox', id, value: e.target.checked})}
-    }),
-    Run(state, send, node, connection, action, action_data.name, id, loading),
-    h('div.foldout_child', [
-      Command(connection, action_data.commands.run),
-      action_data.description ? action_data.description : undefined,
-      h('div.targets', [h('b', 'Valid targets:'), ` ${action_data.hosts.map(HostString).join(', ')}.`]),
-      result_data && result_data.result ? h('div.results', [
-        h('div.followup', [
-          `Results from ${TimeAgo(result_data.date)}`, 
-          h(`div.foldout fas fa-${result_data.foldout ? 'chevron-down' : 'chevron-right'} fa-fw`, {
-            on: {click: [send, {type: 'result_foldout', action, hostname: node.hostname, value: !result_data.foldout}]}
-          })
-        ]),
-        ...(result_data.foldout ? result_data.result
-          .map(r => Result(state, action, r, node.node_id, node.hostname, loading, result_data.filter, send)) : [])
-      ]) : undefined
-    ])
-
-  ])
+  }, InnerFoldout(state, send, id, Run(state, send, node, connection, action, action_data.name, id, loading), [
+    Command(connection, action_data.commands.run),
+    action_data.description ? action_data.description : undefined,
+    h('div.targets', [h('b', 'Valid targets:'), ` ${action_data.hosts.map(HostString).join(', ')}.`]),
+    result_data && result_data.result ? h('div.results', [
+      h('div.followup', {
+        on: {click: [send, {type: 'result_foldout', action, hostname: node.hostname, value: !result_data.foldout}]}
+      }, [
+        `Results from ${TimeAgo(result_data.date)}`, 
+        h(`div.foldout fas fa-${result_data.foldout ? 'chevron-down' : 'chevron-right'} fa-fw`)
+      ]),
+      ...(result_data.foldout ? result_data.result
+        .map(r => Result(state, action, r, node.node_id, node.hostname, loading, result_data.filter, send)) : [])
+    ]) : undefined
+  ]))
 }
