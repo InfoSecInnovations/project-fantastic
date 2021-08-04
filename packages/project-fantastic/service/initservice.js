2a7b5a4ea94261b84a116c8005c1e50668119c8c
Sebastian Boutin Blomfield <sebyihi@yahoo.fr>
2021-08-04 16:56:55 -0700

    launch config to run as service in VSCode debugger

--- packages/project-fantastic/service/initservice.js
+++ packages/project-fantastic/service/initservice.js
@@ -1,10 +1,16 @@
 const { Service } = require('node-windows')
 const path = require('path')
 
-const svc = () => new Service({
+const svc = () => {
+  const srv = new Service({
     name: 'Fantastic Test Service',
     description: 'pls ignore',
     script: path.join(__dirname, '..', 'index.js')
-})
+  })
+  // TODO: can we supply credentials for the service account here?
+  return srv
+}
+
+
 
 module.exports = svc
