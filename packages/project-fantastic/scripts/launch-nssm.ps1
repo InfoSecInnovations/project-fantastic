94c7b9122b68fc8b7a7954edc8dcf6063477eac2
Sebastian Boutin Blomfield <sebyihi@yahoo.fr>
2021-08-27 09:41:30 -0700

    NSSM service script prompts for account details

--- packages/project-fantastic/scripts/launch-nssm.ps1
+++ packages/project-fantastic/scripts/launch-nssm.ps1
@@ -27,9 +27,34 @@ if (-not $isInstalled) {
   [Environment]::SetEnvironmentVariable("Path", $env:Path, "Machine")
 }
 
+### create service
 $serviceName = "Fantastic Test Service"
 $nodePath = "C:\Program Files\nodejs\node.exe"
 $serviceDir = Get-Location
 nssm install $serviceName $nodePath .\node_modules\@infosecinnovations\project-fantastic\index.js
 nssm set $serviceName AppDirectory $serviceDir
+
+### get account options
+[int]$accountOption = 0
+while ($accountOption -lt 1 -or $accountOption -gt 3) {
+  $accountOption = Read-Host "Which account will run the service?
+1 - Currently logged in user
+2 - Enter account credentials
+3 - Managed service account
+"
+}
+if ($accountOption -eq 2) {
+  $username = Read-Host "Service account username"
+  $password = Read-Host "Service account password" -MaskInput
+  nssm set $serviceName ObjectName $username $password
+}
+if ($accountOption -eq 3) {
+  $username = Read-Host "Managed Service Account name"
+  if (-not $username.EndsWith('$')) {
+    $username = $username + '$'
+  }
+  nssm set $serviceName ObjectName $username " "
+}
+
+### launch the service
 nssm start $serviceName
