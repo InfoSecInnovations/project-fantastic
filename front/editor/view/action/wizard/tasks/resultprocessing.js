bbf7147da520f0695bc181200e06409f3e5814f4
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2022-02-11 14:42:25 +0100

    fix result processing wizard task

--- front/editor/view/action/wizard/tasks/resultprocessing.js
+++ front/editor/view/action/wizard/tasks/resultprocessing.js
@@ -4,7 +4,7 @@ import WizardView from '../wizardview'
 export default (state, send) => {
   const enabled = !!state.action.json.functions[state.action.wizard.funcName].result
   const nextTasks = enabled && ['convert_to_json', 'result_label', 'add_data_items']
-  if (enabled && Object.keys(state.action.json.functions).length > state.action.wizard.funcName == 'run' ? 1 : 2) nextTasks.push('followup_actions')
+  if (enabled && (Object.keys(state.action.json.functions).length > state.action.wizard.funcName == 'run' ? 1 : 2)) nextTasks.push('followup_actions')
   return WizardView(
     state, 
     send, 
