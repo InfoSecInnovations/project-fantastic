6dd9f940ad910d6314129ba501cf58f58153e19d
Sebastian Boutin Blomfield <7989595+sebovzeoueb@users.noreply.github.com>
2022-05-25 16:08:33 +0200

    put inventory data in database

--- packages/project-fantastic/inventory/runinventory/createinventoryitems.js
+++ packages/project-fantastic/inventory/runinventory/createinventoryitems.js
@@ -2,7 +2,10 @@ const GetPackagedData = require("../../util/getpackageddata")
 
 const createInventoryItems = async inventory_data => inventory_data ? Promise.all(Object.entries(inventory_data)
   .filter(v => v[1] != 'disabled')
-  .map(v => GetPackagedData(v[0], 'inventory'))) : // TODO: filter out invalid scripts and warn the user
+  .map(async v => {
+    const data = await GetPackagedData(v[0], 'inventory')
+    return {key: v[0], data}
+  })) : // TODO: filter out invalid scripts and warn the user
   Promise.resolve()
 
 module.exports = createInventoryItems
--- packages/project-fantastic/inventory/runinventory/index.js
+++ packages/project-fantastic/inventory/runinventory/index.js
@@ -1,3 +1,4 @@
+const DB = require('../../db')
 const PwshFunction = require('../../util/pwshfunction')
 const CreateInventoryItems = require('./createinventoryitems')
 const GetHosts = require('./gethosts')
@@ -12,11 +13,17 @@ const runInventory = async get_inventory_data => {
     console.log('-----getting hosts for inventory scanning...-----')
     const hosts = await GetHosts()
     console.log('-----running inventory scans on hosts...-----')
+    const date = Date.now()
     for (const node of hosts) {
       for (const item of inventory_items) {
-        const output = await PwshFunction(item.run)(item.run.command, node.access == 'local' ? null : node.hostname)
-        if (output) console.log(output)
-        // TODO: insert data into database
+        const output = await PwshFunction(item.data.run)(item.data.run.command, node.access == 'local' ? null : node.hostname)
+        .then(res => res.map(v => typeof v == 'object' ? JSON.stringify(v) : v))
+        // find matching database records
+        const existing = await DB.all({table: 'inventory_data', columns: ['inventory_data_id', 'data'], conditions: {groups: [{columns: {item_name: item.key}}, {columns: {data: output}, compare: 'IN'}]} })
+        // update existing
+        await DB.update({table: 'inventory_data', row: {date}, conditions: {columns: {inventory_data_id: existing.map(v => v.inventory_data_id)}, compare: 'IN'}})
+        // insert new
+        await DB.insert('inventory_data', output.filter(v => !existing.some(e => v == e.data)).map(v => ({date, data: v, item_name: item.key})))
       }
     }
     loop()
